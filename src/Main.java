099879fd0b65cbcfcc4c1c6e2ec947099bbe1968
MATYAD <matysadam1@gmail.com>
2022-09-17 17:14:52 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        ArrayList<City> cities = new ArrayList<>();
+        cities.add(new City("Praha", 1500000));
+        cities.add(new City( "Budějovice",250000));
+        cities.add(new City("Brno",400000));
+
+        Collections.sort(cities,Collections.reverseOrder());
+        System.out.println(cities);
+
     }
+
+
+
+
+
 
 
 }
